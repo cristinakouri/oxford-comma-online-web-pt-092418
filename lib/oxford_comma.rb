d54d13bc17de002a6fc9fc495304a4040a07cc0c
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-02 17:03:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
 #takes an argument of an array and converts to a string with comma delimiter 
 def oxford_comma(array)
-  if array.length 
-  
+  if array.length == 1
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length >= 3 
+    last = " and #{array[-1]}"
+    array.pop
+    array.join(", ") << last
+  end
 end
-
